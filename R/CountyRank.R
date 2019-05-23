d17e10794b3d45036290736536ca407ef79f78fa
ABSOD <32849428+ABSOD@users.noreply.github.com>
2019-05-23 16:02:04 -0500

    Add files via upload

--- R/CountyRank.R
+++ R/CountyRank.R
@@ -10,19 +10,26 @@
 
 
 CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
+ 
   withProgress(message = 'Generating County Ranking', value = 0, {  # Initialize Progress bar
+
   sYr <- 2010
   state = "08"
   allCty <- CtyList$countyfips
-  allCty0 <- formatC(allCty, width = 3, format = "d", flag = "0")
-  
+
   f.ctyfull <- CtyList[,c(1,3)]
+  names(f.ctyfull) <- c("GEOID","County")
+  f.ctyfull$GEOID <- paste0(state,formatC(f.ctyfull$GEOID, width = 3, format = "d", flag = "0"))
   colName2 <- c("County" = "name")
+  
   #Total Population
   if("totpop" %in% chkList){
     f.ctypop <- CtyList[,c(1,5)]
     
-    f.ctyfull <- inner_join(f.ctyfull,f.ctypop,by="countyfips")
+    names(f.ctypop) <- c("GEOID","totalpopulation")
+    f.ctypop$GEOID <- paste0(state,formatC(f.ctypop$GEOID, width = 3, format = "d", flag = "0"))
+    
+    f.ctyfull <- inner_join(f.ctyfull,f.ctypop,by="GEOID")
     popnames <- c("Total Population" = "totalpopulation")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -31,22 +38,22 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   # Population Growth Rate
   if("popgr" %in% chkList) {
     yrs <- as.character(c(sYr,eYr))
-    f.popGr <- data.frame()
-    for(i in 1:length(allCty)) {
-      ctyPopGrowth <- paste0("SELECT countyfips, year, totalpopulation FROM estimates.county_muni_timeseries WHERE countyfips = ",allCty[i]," AND placefips = 0;") 
-      mCty <- dbGetQuery(DBPool,ctyPopGrowth) %>%
-        filter(year %in% yrs)
-      f.popGr <- bind_rows(f.popGr,mCty)
-    }
+
+      ctyPopGrowth <- paste0("SELECT countyfips, year, totalpopulation FROM estimates.county_muni_timeseries WHERE countyfips > 0 and placefips = 0;") 
+     
+    f.popGR <- dbGetQuery(DBPool,ctyPopGrowth)  %>% filter(year %in% yrs)
+    names(f.popGR)[1] <- "GEOID"
+    f.popGR$GEOID <- paste0(state,formatC(f.popGR$GEOID, width = 3, format = "d", flag = "0"))
     
-    f.ctyGR <- f.popGr %>%   
-      arrange(countyfips,year)%>%
-      group_by(countyfips) %>%
+    f.ctyGR <- f.popGR %>%   
+      arrange(GEOID,year)%>%
+      group_by(GEOID) %>%
       mutate(year=as.numeric(year),
              totalpopulation=as.numeric(totalpopulation),
-             growthRate=signif((((totalpopulation/lag(totalpopulation))^(1/(year-lag(year)))) -1)*100))
-    f.ctyGR <-  f.ctyGR[which( f.ctyGR$year == eYr),]  
-    f.ctyfull <- inner_join(f.ctyfull,f.ctyGR[,c(1,4)],by="countyfips")
+             growthRate=signif((((totalpopulation/lag(totalpopulation))^(1/(year-lag(year)))) -1)*100)) %>%
+             filter(year == eYr)
+    
+    f.ctyfull <- inner_join(f.ctyfull,f.ctyGR[,c(1,4)],by="GEOID")
     
     popnames <- c("Average Population Growth Rate" = "growthRate")
     colName2 <- c(colName2,popnames)
@@ -60,21 +67,23 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   f.agesya$age2564 <- ifelse(f.agesya$age >= 25 & f.agesya$age < 65,1,0)
   f.agesya$age65 <- ifelse(f.agesya$age >= 65,1,0)
   f.agesya$totalpopulation <- as.numeric(f.agesya$totalpopulation)
+  names(f.agesya)[1] <- "GEOID"
+  f.agesya$GEOID <- paste0(state,formatC(f.agesya$GEOID, width = 3, format = "d", flag = "0"))
   
   f.ageTot <- f.agesya %>%
-    group_by(countyfips) %>%
+    group_by(GEOID) %>%
     summarize(tpop = sum(totalpopulation))
   
   if("pop2564" %in% chkList) {
     f.age2564 <- f.agesya %>%
-      group_by(countyfips, age2564) %>%
+      group_by(GEOID, age2564) %>%
       summarize(tpop2564 = sum(totalpopulation))
     f.age2564 <- f.age2564[which(f.age2564$age2564 == 1),]
     
-    f.age2564pct <- inner_join(f.age2564, f.ageTot,by="countyfips")
+    f.age2564pct <- inner_join(f.age2564, f.ageTot,by="GEOID")
     f.age2564pct$tpop2564pct <- f.age2564pct$tpop2564/f.age2564pct$tpop
     
-    f.ctyfull <- inner_join(f.ctyfull,f.age2564pct[,c(1,5)], by="countyfips")
+    f.ctyfull <- inner_join(f.ctyfull,f.age2564pct[,c(1,5)], by="GEOID")
     
     popnames <- c("Percent of Population Age 25 to 64" = "tpop2564pct")
     colName2 <- c(colName2,popnames)
@@ -83,15 +92,15 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   
   if("pop65" %in% chkList) {
     f.age65 <- f.agesya %>%
-      group_by(countyfips, age65) %>%
+      group_by(GEOID, age65) %>%
       summarize(tpop65 = sum(totalpopulation))
     f.age65 <- f.age65[which(f.age65$age65 == 1),]
     
-    f.age65pct <- inner_join(f.age65, f.ageTot,by="countyfips")
+    f.age65pct <- inner_join(f.age65, f.ageTot,by="GEOID")
     f.age65pct$tpop65pct <- f.age65pct$tpop65/f.age65pct$tpop
     
     
-    f.ctyfull <- inner_join(f.ctyfull,f.age65pct[,c(1,5)], by="countyfips")
+    f.ctyfull <- inner_join(f.ctyfull,f.age65pct[,c(1,5)], by="GEOID")
     popnames <- c("Percent of Population Age 65 and Older" = "tpop65pct")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -100,22 +109,20 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   #percent non-white
   
   if("pctNW" %in% chkList) {
-    f.nonwhite <- data.frame()
-    for(i in 1:length(allCty0)) {
-      ACSRaceCTY <- codemog_api(data="b03002", db=ACS, geonum=paste0("1", state, allCty0[i]),meta="no")
-      f.nonwhite <- bind_rows(f.nonwhite,ACSRaceCTY)
-    }
-    f.nonwhite[,c(3,8:28)] <-  lapply(f.nonwhite[,c(3,8:28)], function(x) as.numeric(as.character(x)))
-    names(f.nonwhite)[3] <- "countyfips"
-    
+    f.nonwhite <-codemog_api(data="b03002",db=ACS,sumlev="50",geography="sumlev",meta="no")
+    # Conver to numeric
+    f.nonwhite[,8:ncol(f.nonwhite)] <- sapply(f.nonwhite[,8:ncol(f.nonwhite)],as.numeric)
+    # Change geonum to geoid
+    names(f.nonwhite)[7] <- "GEOID"
+    f.nonwhite$GEOID <- substr(f.nonwhite$GEOID,2,nchar(f.nonwhite$GEOID))
+  
     f.nonwhite <- f.nonwhite %>%
-      select(geoname:b03002012) %>%
-      mutate(TotalPop=b03002001,
-             NHWhite=b03002003,
+        mutate(TotalPop= b03002001,
+             NHWhite= b03002003,
              pctNonWhite = (TotalPop - NHWhite)/TotalPop)
     
-    f.nonwhite <- f.nonwhite[,c(3,22)]
-    f.ctyfull <- inner_join(f.ctyfull, f.nonwhite, by="countyfips")
+    f.nonwhite <- f.nonwhite[,c(7,31)]
+    f.ctyfull <- inner_join(f.ctyfull, f.nonwhite, by="GEOID")
     popnames <- c("Percent of Non-White Persons" = "pctNonWhite")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -125,21 +132,20 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   # Number of people with a bachelor's degree or higher
   #county Education Value
   if("educ" %in% chkList) {
-    d13cty <- data.frame()
-    for(i in 1: length(allCty0)) {
-      x <- codemog_api(data="b15003",db=ACS,geonum=paste("1", state , allCty0[i], sep=""),meta="no")
-      d13cty <- bind_rows(d13cty,x)
-    }
+    f.educ <- codemog_api(data="b15003",db=ACS,sumlev="50",geography="sumlev",meta="no")
     
-    d13cty[,c(3,8:32)] <-  lapply(d13cty[,c(3,8:32)], function(x) as.numeric(as.character(x)))
-    names(d13cty)[3] <- "countyfips"
+    # Conver to numeric
+    f.educ[,8:ncol(f.educ)] <- sapply(f.educ[,8:ncol(f.educ)],as.numeric)
+    # Change geonum to geoid
+    names(f.educ)[7] <- "GEOID"
+    f.educ$GEOID <- substr(f.educ$GEOID,2,nchar(f.educ$GEOID))
     
-    f.educVal <- d13cty %>% group_by(countyfips) %>%
-      summarise(total = b15003001,
-                baplus =  b15003022 + b15003023+b15003024+b15003025) %>%
-      mutate(bapct = baplus/total)
+    f.educVal <- f.educ %>% 
+     mutate(total = b15003001,
+            baplus =  b15003022 + b15003023 + b15003024 + b15003025,
+            bapct = baplus/total)
     
-    f.ctyfull <- inner_join(f.ctyfull, f.educVal[,c(1,4)], by="countyfips")
+    f.ctyfull <- inner_join(f.ctyfull, f.educVal[,c(7,35)], by="GEOID")
     popnames <- c("Percentage of Persons with a Bachelor's Degree or Higher" = "bapct")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -147,16 +153,14 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   
   # Total Estimated Jobs
   if("jobs" %in% chkList) {
-    f.ctyJobs <- data.frame()
-    for(i in 1: length(allCty)) {
-      sqlStrJobs <- paste0("SELECT area_code, population_year, total_jobs FROM estimates.jobs_by_sector WHERE area_code = ",allCty[i]," and population_year = ",eYr,
+      sqlStrJobs <- paste0("SELECT area_code, population_year, total_jobs FROM estimates.jobs_by_sector WHERE area_code > 0 and population_year = ",eYr,
                            " and sector_id = '0';")
-      f.Jobs <- dbGetQuery(DBPool, sqlStrJobs)
-      f.ctyJobs <- bind_rows(f.ctyJobs,f.Jobs)
-    }
-    names(f.ctyJobs)[1] <- "countyfips" 
+      f.ctyJobs <- dbGetQuery(DBPool, sqlStrJobs)
+   
+    names(f.ctyJobs)[1] <- "GEOID" 
+    f.ctyJobs$GEOID <- paste0(state,formatC(f.ctyJobs$GEOID, width = 3, format = "d", flag = "0"))
     
-    f.ctyfull <- inner_join(f.ctyfull, f.ctyJobs[,c(1,3)], by="countyfips")
+    f.ctyfull <- inner_join(f.ctyfull, f.ctyJobs[,c(1,3)], by="GEOID")
     popnames <- c("Total Estimated Jobs"= "total_jobs")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -164,16 +168,17 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   
   # Median Household Income
   if("medinc" %in% chkList) {
-    f.ctymedinc <- data.frame()
-    for(i in 1: length(allCty0)) { 
-      hhincc <- codemog_api(data="b19013",db=ACS, geonum=paste0("1", state, allCty0[i]), meta="no")
-      f.ctymedinc <- bind_rows(f.ctymedinc,hhincc)
-    }
-    f.ctymedinc <-   f.ctymedinc %>% mutate(medianinc = as.numeric(b19013001))
-    names(f.ctymedinc)[3] <- "countyfips"
-    f.ctymedinc$countyfips <- as.numeric(f.ctymedinc$countyfips)
+    f.ctymedinc <- codemog_api(data="b19013",db=ACS,sumlev="50",geography="sumlev",meta="no")
+ 
+    # Conver to numeric
+    f.ctymedinc[,8:ncol(f.ctymedinc)] <- sapply(f.ctymedinc[,8:ncol(f.ctymedinc)],as.numeric)
+    # Change geonum to geoid
+    names(f.ctymedinc)[7] <- "GEOID"
+    f.ctymedinc$GEOID <- substr(f.ctymedinc$GEOID,2,nchar(f.ctymedinc$GEOID))
     
-    f.ctyfull <- inner_join(f.ctyfull,f.ctymedinc[,c(3,9)],by="countyfips")
+    f.ctymedinc <-   f.ctymedinc %>% mutate(medianinc = b19013001)
+
+    f.ctyfull <- inner_join(f.ctyfull,f.ctymedinc[,c(7,9)],by="GEOID")
     popnames <- c("Median Household Income"= "medianinc")
     colName2 <- c(colName2,popnames)
     incProgress()
@@ -182,27 +187,28 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   
   # % in poverty
   if("poverty" %in% chkList) {
-    f.povertyr <- data.frame()
-    for(i in 1:length(allCty0)) {
-      povertyc <- codemog_api(data="b17001",db=ACS, geonum=paste0("1", state, allCty0[i]), meta="no")
-      f.povertyr <- bind_rows(f.povertyr,povertyc)
-    }
-    names(f.povertyr)[3]  <- "countyfips"
-    f.povertyr$countyfips <- as.numeric(f.povertyr$countyfips)
+    f.povertyr <- codemog_api(data="b17001",db=ACS,sumlev="50",geography="sumlev",meta="no")
+
+    # Conver to numeric
+    f.povertyr[,8:ncol(f.povertyr)] <- sapply(f.povertyr[,8:ncol(f.povertyr)],as.numeric)
+    # Change geonum to geoid
+    names(f.povertyr)[7] <- "GEOID"
+    f.povertyr$GEOID <- substr(f.povertyr$GEOID,2,nchar(f.povertyr$GEOID))
+    
     
     f.povertypct <- f.povertyr %>% 
-      group_by(countyfips) %>%
-      summarize(npoverty = sum(as.numeric(b17001002)), tpop = sum(as.numeric(b17001001))) %>%
-      mutate(povpct = (npoverty/tpop)) 
+      mutate(npoverty = b17001002,
+       tpop = b17001001,
+      povpct = (npoverty/tpop)) 
     
-    f.ctyfull <- inner_join(f.ctyfull,f.povertypct[,c(1,4)],by="countyfips")
+    f.ctyfull <- inner_join(f.ctyfull,f.povertypct[,c(7,69)],by="GEOID")
     popnames <- c("Percent of Persons Below the Poverty Line"= "povpct")
     colName2 <- c(colName2,popnames)
     incProgress()
   }
   
   
-  
+
   #Standardizing and Formatting
   f.ctyfull$aggVal <- 0  
   if("totpop" %in% chkList) {
@@ -258,13 +264,13 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
     f.ctyfull$povpct <- percent(f.ctyfull$povpct*100)
     f.ctyfull$aggVal <- f.ctyfull$aggVal + f.ctyfull$povpctZ
   }  
-  f.ctyfull$aggRank <- 65 - rank(f.ctyfull$aggVal)  
+  f.ctyfull$aggRank <- (nrow(f.ctyfull) + 1) - rank(f.ctyfull$aggVal)  
   popnames <- c("Aggregate Rank"= "aggRank")
   colName2 <- c(colName2,popnames)
   
   # extracting AggVal
-  f.ctyAggVal <- f.ctyfull[,c("municipalityname","aggVal")]
-  names(f.ctyAggVal) <- c("name","r")
+  f.ctyAggVal <- f.ctyfull[,c("County","aggVal")]
+  names(f.ctyAggVal)[2] <- "r"
   f.ctyAggVal$r <- round(abs(f.ctyAggVal$r),digits=2) + 1
   incProgress()
   
@@ -298,22 +304,22 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   fin_clus <- as.data.frame(cutree(f.clusStr,k=NClus[1,1]))
   cty_clus <- cbind(f.ctynames,fin_clus)
   rownames(cty_clus) <- c()
-  names(cty_clus) <- c("name","cluster")
-  cty_clus$name  <- as.character(cty_clus$name)
+  names(cty_clus) <- c("County","cluster")
+  cty_clus$County <- as.character(cty_clus$County)
   
   
-  f.ctyfinclus <- inner_join(cty_clus,f.ctyAggVal, by="name") 
+  f.ctyfinclus <- inner_join(cty_clus,f.ctyAggVal, by="County") 
   incProgress()
 
   #Outputting Circle Packing Diagram
   
-  f.ctyfinclus$name <- gsub(" County","",f.ctyfinclus$name)
+  f.ctyfinclus$County <- gsub(" County","",f.ctyfinclus$County)
   
   
   # Output data tree
   f.ctyfinclus$clusterName <- paste0("Cluster ",f.ctyfinclus$cluster)
   
-  f.ctyfinclus$pathString <- paste("state", f.ctyfinclus$clusterName, f.ctyfinclus$name, sep="/")
+  f.ctyfinclus$pathString <- paste("state", f.ctyfinclus$clusterName, f.ctyfinclus$County, sep="/")
   f.tree <-as.Node(f.ctyfinclus)
   
 
@@ -322,8 +328,8 @@ CountyRank <- function(DBPool, CtyList, chkList, eYr, ACS) {
   
 # outputting DT ranking Table  
 
-names(f.ctyTab)[1] <- "name"
-f.ctyTab <- inner_join(cty_clus,f.ctyTab, by="name") 
+names(f.ctyTab)[1] <- "County"
+f.ctyTab <- inner_join(cty_clus,f.ctyTab, by="County") 
 f.ctyTab <- f.ctyTab[order(f.ctyTab$aggRank),]
 popnames2 <- c("Cluster" = "cluster")
 popnames1 <- colName2[1]
